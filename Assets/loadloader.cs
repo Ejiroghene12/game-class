9a5f303f8e211fef22ffecd27c0be89ecebc5f84
Jude <ei183@live.mdx.ac.uk>
2021-11-08 13:55:06 +0000

    Start

---
+++ Assets/loadloader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadloader : MonoBehaviour
+{
+    public int ILevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToloadLevel = false;
+  
+  void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+
+        void LoadScene()
+        {
+            if (useIntegerToloadLevel)
+            {
+                SceneManager.LoadScene(ILevelToLoad);
+            }
+            else
+            {
+                SceneManager.LoadScene(sLevelToLoad);
+            }
+
+        }
+
+    }
+}
