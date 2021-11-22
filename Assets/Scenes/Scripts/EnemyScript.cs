a54a452b88f2c1a2b0fe36f3adcb4ee5b20d4a5b
jude <eI183@LIVE.MDX.AC.UK>
2021-11-22 13:31:16 +0000

    new movement

--- Assets/Scenes/Scripts/EnemyScript.cs
+++ Assets/Scenes/Scripts/EnemyScript.cs
@@ -1,13 +1,16 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public GameObject Player, checkpoint;
+    public GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
-
+        checkpoint = GameObject.FindWithTag("StartLevel");
+        camera = GameObject.FindWithTag("MainCamera");
     }
 
     // Update is called once per frame
@@ -20,8 +23,11 @@ public class EnemyScript : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
-
+            Destroy(collision.gameObject); //destroy player character
+           GameObject newplayer=Instantiate(Player, checkpoint.transform.position, Quaternion.identity); //spawn a new player character at checkpoint position
+            camera.GetComponent<FollowPlayer>().gameObjectFollowedByCamera = newplayer; //set object to follow on camera
+            newplayer.GetComponent<BoxCollider2D>().enabled = true;
+            newplayer.GetComponent<Movement>().enabled = true;
             print("Killed");
         }
     }
--- Assets/Scenes/Scripts/Movement.cs
+++ Assets/Scenes/Scripts/Movement.cs
@@ -19,12 +19,12 @@ public class Movement : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetMouseButtonDown(0) && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
 
-       // { DontDestroyOnLoad(gameObject); }
+        // { DontDestroyOnLoad(gameObject); }
     }
 
   
